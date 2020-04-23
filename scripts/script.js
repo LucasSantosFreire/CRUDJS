869680c1b36411d40966bb385a6b852b7f529196
Lucas Santos <lucas-3691@hotmail.com>
2020-04-23 16:53:48 -0300

    adicinado barra de pesquisa

--- scripts/script.js
+++ scripts/script.js
@@ -128,3 +128,22 @@ function Farol(valor){
         document.getElementById("farolColor").classList.remove("color2");
     }
 }
+
+function Pesquisa() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tabela");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
